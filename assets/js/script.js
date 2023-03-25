9cb43a410334d934a3df8873eca8e0f5ee23e8d2
Irina <irkag22@gmail.com>
2023-03-25 11:45:30 -0400

    worked on script.js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,127 @@
-var startBtn = document.querySelector("button");
-var 
-    
-startbtn.addEventListener("click", function(){
+var startBtn = document.querySelector("#startBtn");
+var startContainer = document.querySelector("#start-container");
+var questionContainer = document.querySelector("#question-container");
+var saveBtn = document.querySelector("#saveBtn");
+var questionIndex = 0;
 
-})
-    
-    
-    var questions = [
-        {
-            question: "What does HTML stand for?",
-            choices: ["How To Maintain Love", "Hyper Text Martian Language", "Hyper Text Markup Language"],
-            answer: "Hyper Text Markup Language",
-        },
+var questions = [
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "How To Maintain Love",
+      "Hyper Text Martian Language",
+      "Hyper Text Markup Language",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
 
-        {
-            question: "What does CSS stand for?",
-            choices: ["Cats Stay Sleeping", "Coding Social Skills", "Cascading Style Sheets"],
-            answer: "Cascading style sheets",
-        }
+  {
+    question: "What does CSS stand for?",
+    choices: [
+      "Cats Stay Sleeping",
+      "Coding Social Skills",
+      "Cascading Style Sheets",
+    ],
+    answer: "Cascading Style Sheets",
+  },
 
-        {
-            question: "What does === mean in JS?",
-            choices:  ["Triple Dash", "Border Style", "Strict Equality"],
-            answer: "Strict Equality",
-        }
+  {
+    question: "What does === mean in JS?",
+    choices: ["Triple Dash", "Border Style", "Strict Equality"],
+    answer: "Strict Equality",
+  },
 
-        {
-            question: "What is a Boolean value?",
-            choices: ["T/F value", "Color attribute", "Decimals"],
-            answer: "T/F value",
-        }
+  {
+    question: "What is a Boolean value?",
+    choices: ["T/F value", "Color attribute", "Decimals"],
+    answer: "T/F value",
+  },
 
-        {
-            question: "What does JavaScript do?",
-            choices: ["Makes the website dyanamic", "Creates pop=up boxes", "Assigns border margins"],
-            answer: "Makes the website dynamic",
-        }
-    ]
+  {
+    question: "What does JavaScript do?",
+    choices: [
+      "Makes the website dyanamic",
+      "Creates pop=up boxes",
+      "Assigns border margins",
+    ],
+    answer: "Makes the website dyanamic",
+  },
+];
 
-    var random = questions.[Math.floor(Math.random()]
-        // ^ DOES THIS MAKE SENSE?
+setInterval(function () {}, 5000);
+clearInterval();
 
-    setInterval(function(){
+function startQuiz() {
+  startContainer.innerHTML = "";
+  displayQuestion();
+}
 
-    }, 5000)
+function displayQuestion() {
+  var currentQuestion = questions[questionIndex];
 
-    clearInterval(); 
+  // create the element using js
+  var questionHeader = document.createElement("h2");
 
+  // setting the text value of the created element -- in this case the question H2
+  questionHeader.innerText = currentQuestion.question;
 
+  //appending the newly created element with the assigned text to the container
+  questionContainer.appendChild(questionHeader);
 
+  // generating the button
+  for (let index = 0; index < currentQuestion.choices.length; index++) {
+    var answerButton = document.createElement("button");
+    answerButton.innerText = currentQuestion.choices[index];
 
+    var checkAnswer = function (event) {
+      //  if we reached the end of the startQuiz, we're gonna run the "endGame" function
+      if (questionIndex >= questions.length - 1) return endGame();
 
+      // we're trying to match the inner text of our button target
+      // to the expected answer for the current question
+      if (event.target.innerText === currentQuestion.answer) {
+        // increase q index by 1
+        questionIndex++;
 
-white_check_mark
-eyes
-raised_hands
+        questionContainer.innerHTML = "";
+        displayQuestion();
+      }
+    };
 
+    answerButton.addEventListener("click", checkAnswer);
 
+    questionContainer.append(answerButton);
+  }
+}
 
+function endGame() {
+  console.log("finished");
+  // do logic here for when you finish the game, maybe log the score, and after that redirect to the highscores page/ component
+}
 
-//click button to start
-    //event listener
-    //a timer will start
-//user is presented with a question
-//user selects one multiple choice question
-    //IF answer is wrong, decrement the timer
-        //Display that the user choice was incorrect, then move to next question
-    //IF answer is correct
-        //Display that the user choice was correct(textContent)
-        //Increment the score++
-    //FOR loop to loop through the questions array
-//When timer hits 0 OR no more questions in array
-    //THEN clear interval
-    //THEN allow user to input initials
-    //THEN link to highscores page(two separate HTML pages, index.html and hiscores.html)
-    //Save data to local storage(setItem, getItem)
+function renderLastScore() {
+  var lastScore = JSON.parse(localStorage.getItem("userScore"));
+  if (lastScore !== null) {
+    document.getElementById("saved-initials").innerHTML = lastScore.user;
+    document.getElementById("saved-score").innerHTML = lastScore.score;
+  } else {
+    return;
+  }
+}
 
-//startQuiz() function changes content
-    //hide the start screen
-    //unhide the questions section
-    //start the timer
-    //show the starting time
-    //call getQuestion()
-//getQuestion() function get current question object from questions array
-    //change content of the page
-    //getElementbyId, change textContent
-    //loop over choices create a button for each choice
-    //after creating all the elements we appendChild
-//questionClick() function that checks choice user clicked
-    //first we check that the event.target matches an answer choice
-    //check event.target.value matches the questions[currentIndex].answer
+function init() {
+  renderLastScore();
+}
+init();
 
+startBtn.addEventListener("click", startQuiz);
+answerButton.addEventListener("click", checkAnswer);
+saveBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  saveLastGrade();
+  renderLastGrade();
+});
 
-
-
-
-
-
-
-
-
+// timer
+// collecting the user name and the timer value as the score
+// saving that to ls
